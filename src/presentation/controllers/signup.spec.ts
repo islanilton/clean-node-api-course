d3e212a888a37d6a76f674cc40c7ecb519d9c8ea
Islanilton <islanilton.rodrigues@gmail.com>
2022-04-08 15:42:54 -0300

    test: ensure SignupController returns 400 if no password is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -28,4 +28,17 @@ describe('SignUp Controller', () => {
     expect(httpRespose.statusCode).toBe(400)
     expect(httpRespose.body).toEqual(new MissingParamError('email'))
   })
+  test('Should return 400 if no password is provided', () => {
+    const sut = new SingupController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpRespose = sut.handle(httpRequest)
+    expect(httpRespose.statusCode).toBe(400)
+    expect(httpRespose.body).toEqual(new MissingParamError('password'))
+  })
 })
